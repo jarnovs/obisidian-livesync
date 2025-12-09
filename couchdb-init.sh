dac3e05e22727d164cc6a467a8e8b78fc0c30feb
server <server@usenov.com>
2025-12-09 20:45:22 +0000

    initial

---
+++ couchdb-init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+export $(grep -v '^#' .env | xargs)
+
+if [[ -z "$HOSTNAME" ]]; then
+    echo "ERROR: Hostname missing"
+    exit 1
+fi
+if [[ -z "$COUCHDB_USER" ]]; then
+    echo "ERROR: Username missing"
+    exit 1
+fi
+
+if [[ -z "$COUCHDB_PASSWORD" ]]; then
+    echo "ERROR: Password missing"
+    exit 1
+fi
+if [[ -z "$node" ]]; then
+    echo "INFO: defaulting to _local"
+    node=_local
+fi
+
+echo "-- Configuring CouchDB by REST APIs... -->"
+
+until (curl -X POST "${HOSTNAME}/_cluster_setup" -H "Content-Type: application/json" -d "{\"action\":\"enable_single_node\",\"username\":\"${COUCHDB_USER}\",\"password\":\"${COUCHDB_PASSWORD}\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"singlenode\":true}" --user "${COUCHDB_USER}:${password}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/chttpd/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/chttpd_auth/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/httpd/WWW-Authenticate" -H "Content-Type: application/json" -d '"Basic realm=\"couchdb\""' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/httpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/chttpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/chttpd/max_http_request_size" -H "Content-Type: application/json" -d '"4294967296"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/couchdb/max_document_size" -H "Content-Type: application/json" -d '"50000000"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/cors/credentials" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+until (curl -X PUT "${HOSTNAME}/_node/${node}/_config/cors/origins" -H "Content-Type: application/json" -d '"app://obsidian.md,capacitor://localhost,http://localhost"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"); do sleep 5; done
+
+echo "<-- Configuring CouchDB by REST APIs Done!"
